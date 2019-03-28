9358591e01a96d2c2bbf22f0ebc652a5391199cc
Joao Campos <jota.fields@gmail.com>
2019-03-28 18:43:19 +0000

    feito deireito

---
+++ Ficha01/ex04_5.js
@@ -0,0 +1,8 @@
+/* 4. Tipos de dados:
+a. Qual o output da seguinte script:
+let name = "Ilya";
+alert( `hello ${1}` ); // hello 1
+alert( `hello ${"name"}` ); // hello name
+alert( `hello ${name}` ); // ? hello Ilya*/
+
+
--- Ficha03/ex05.js
+++ Ficha03/ex05.js
@@ -3,28 +3,31 @@ const frmGame = document.querySelector("#frmGame") //ou ("form")
 frmGame.addEventListener("submit", function (event) {
     const txtGameYear = document.querySelector("#txtGameYear").value
     const currentYear = new Date().getFullYear()
-    if (txtGameYear > 1950 && txtGameYear < currentYear) {
+    if (txtGameYear > 1950 && txtGameYear <= currentYear) {
         alert("Validaçao OK")
         addGameTable()
+        frmGame.reset()
     } else {
         alert("erro")
     }
+    //addGameTable();
     event.preventDefault() //colocar um parametro na function em cima para poder fazer isto
 })
 
 function addGameTable() {
     const table = document.querySelector("table")
-    const gameName = document.querySelector("#txtGameName")
-    const gameYear = document.querySelector("#txtGameYear")
-    const gameGenre = document.querySelector("#txtGameGenre")
-    const gameCover = document.querySelector("#txtGameCover")
+    const gameName = document.querySelector("#txtGameName").value
+    const gameYear = document.querySelector("#txtGameYear").value
+    const gameGenre = document.querySelector("#txtGameGenre").value
+    const gameCover = document.querySelector("#txtGameCover").value
 
+    
     table.innerHTML += `
     <tr>
         <td>${gameName}</td>
         <td>${gameYear}</td>
         <td>${gameGenre}</td>
-        <td><img src="${gameCover}/></td>
+        <td><img src="${gameCover}"/></td>
         <td></td>
         
         </tr>
