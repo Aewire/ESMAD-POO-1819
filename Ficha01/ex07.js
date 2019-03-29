c54a850b5e93610077003b1e4a3727ab353f618b
Joao Campos <jota.fields@gmail.com>
2019-03-29 06:36:08 +0000

    gsdg

---
+++ Ficha01/ex07.js
@@ -0,0 +1,6 @@
+/* Interação */
+/* crie uma instrução que solicite um nome e imprima o nome juntando
+asteriscos antes e depois do nome  */
+
+let name = prompt("qual e o teu nome")
+alert(`"${name}"`)
